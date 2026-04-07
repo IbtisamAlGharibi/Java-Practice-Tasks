e12c298cc487db9952c3d0026fb0380a4aacf420
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 11:00:56 +0400

    Task 3: love6

---
+++ src/Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 4;
+        if ((a == 6) || (b ==6) || (a+b ==6) || (Math.abs(a - b) == 6)){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
+}
