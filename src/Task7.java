19b3dca4ec08c182d7da7ce82d762d519adb6708
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 11:25:02 +0400

    Task 7: twoAsOne

---
+++ src/Task7.java
@@ -0,0 +1,22 @@
+public class Task7 {
+    public static void main(String[] args){
+
+
+        int a= 2;
+        int b = 4;
+        int c = 6;
+
+        if ((a + b == c ) || (a + c == b) || (b + c == a)){
+
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+
+    }
+
+
+
+
+}
