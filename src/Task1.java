17072313041a082258016c88109205f36a647e70
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 10:35:15 +0400

    Task 1: cigarParty

---
+++ src/Task1.java
@@ -0,0 +1,22 @@
+public class Task1 {
+    public static void main(String[] args){
+
+        int cigars = 30;
+        boolean isWeekend = false;
+        if (isWeekend == false){
+            if (cigars>= 40 && cigars <=60){
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
+
+        }else{
+            if(cigars >= 40){
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
+        }
+
+    }
+}
