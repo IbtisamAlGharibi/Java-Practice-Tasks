2c40404384eea2701f609c3562164cd95b416a3c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 11:06:08 +0400

    Task 4: more20

---
+++ src/Task4.java
@@ -0,0 +1,11 @@
+public class Task4 {
+    public static void main(String[] args){
+        int n = 4;
+        if ((n%20 == 1) || (n%20 ==2)){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+    }
+}
