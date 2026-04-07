abee8df813ebcf384f1ea4f07adaca8dfbef7962
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 11:11:18 +0400

    Task 5: nearTen

---
+++ src/Task5.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    public static void main(String[] args){
+        int num = 4;
+        if ((num % 10 ==0) || (num % 10 ==1) || (num % 10 ==2) ||
+                (num % 10 ==8) || (num % 10 ==9)){
+
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+
+        }
+
+
+    }
+}
