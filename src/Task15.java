e310b39768e36ecd24b89bcd22b25926298e95a2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 14:31:38 +0400

    Task 15: teenSum

---
+++ src/Task15.java
@@ -0,0 +1,13 @@
+public class Task15 {
+    public static void main(String[] args){
+        int a = 9;
+        int b = 14;
+
+        if ((a>= 13 && a<=19) || (b>= 13 && b<=19) ){
+            System.out.println("19");
+        }else{
+            System.out.println(a+b);
+        }
+
+    }
+}
