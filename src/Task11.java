006874843c19f7f84dcfcac0445a7fbff9cec54f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 12:29:39 +0400

    Task 11: dateFashion

---
+++ src/Task11.java
@@ -0,0 +1,14 @@
+public class Task11 {
+    public static void main(String[] args){
+        int you = 3;
+        int date = 2;
+        if ((you <= 2) || (date <= 2)){
+            System.out.println("0");
+        } else if ((you >= 8) || (date >= 8)) {
+            System.out.println("2");
+            
+        }else {
+            System.out.println("1");
+        }
+    }
+}
