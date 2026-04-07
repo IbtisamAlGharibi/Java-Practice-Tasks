60150b91750e7c780ad98c6a60f86746fc091144
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-07 12:39:15 +0400

    Task 12: sortaSum

---
+++ src/Task12.java
@@ -0,0 +1,14 @@
+public class Task12 {
+    public static void main(String []args){
+        int a =10;
+        int b = 14;
+
+        int sum = a +b;
+        if (sum>=10 && sum <=19){
+            System.out.println("20");
+        }else {
+            System.out.println(sum);
+        }
+
+    }
+}
